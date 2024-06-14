58139d5ea3563d4b3ddddaeafe2bb28441847180
Divyanshu Aryan <42530432+divyanshu292@users.noreply.github.com>
2024-06-14 16:42:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,123 +1,123 @@
-const mongoose = require("mongoose")
-const Document = require("./Document")
+// const mongoose = require("mongoose")
+// const Document = require("./Document")
 
-mongoose.connect("mongodb://localhost/google-doc-clone", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//    useFindAndModify: false,
-//     useCreateIndex: true,
- })
+// mongoose.connect("mongodb://localhost/google-doc-clone", {
+// //     useNewUrlParser: true,
+// //     useUnifiedTopology: true,
+// //    useFindAndModify: false,
+// //     useCreateIndex: true,
+//  })
 
-const io = require('socket.io')(3001,{
-    cors:{
-        origin: 'google-d-c-c-s.vercel.app',
-        methods: ["GET", "POST"],
-    },
-})
-const defaultValue = ""
-io.on("connection", socket => {
-    socket.on("get-document", async documentId => {
-        const document = await findOrCreateDocument(documentId)
-        socket.join(documentId)
-        socket.emit("load-document", document.data)
+// const io = require('socket.io')(3001,{
+//     cors:{
+//         origin: 'google-d-c-c-s.vercel.app',
+//         methods: ["GET", "POST"],
+//     },
+// })
+// const defaultValue = ""
+// io.on("connection", socket => {
+//     socket.on("get-document", async documentId => {
+//         const document = await findOrCreateDocument(documentId)
+//         socket.join(documentId)
+//         socket.emit("load-document", document.data)
 
-        socket.on('send-changes', delta => {
-            socket.broadcast.to(documentId).emit("receive-changes", delta)
-        })
-        socket.on("save-document", async data => {
-            await Document.findByIdAndUpdate(documentId, {data})
-        })
-    })
+//         socket.on('send-changes', delta => {
+//             socket.broadcast.to(documentId).emit("receive-changes", delta)
+//         })
+//         socket.on("save-document", async data => {
+//             await Document.findByIdAndUpdate(documentId, {data})
+//         })
+//     })
     
-})
+// })
 
-async function findOrCreateDocument(id){
-    if(id==null) return
+// async function findOrCreateDocument(id){
+//     if(id==null) return
 
-    const document = await Document.findById(id)
-    if(document) return document 
-    return await Document.create({_id: id, data: defaultValue})
-}
+//     const document = await Document.findById(id)
+//     if(document) return document 
+//     return await Document.create({_id: id, data: defaultValue})
+// }
 
 
 //--------------------------------------Some libraries and methods are deprecated above----------------
-// const mongoose = require("mongoose");
-// const Document = require("./Document");
+const mongoose = require("mongoose");
+const Document = require("./Document");
 
-// //mongoose.connect("mongodb://localhost/google-doc-clone", {
-//     // useNewUrlParser: true,
-//     // useUnifiedTopology: true,
-//     // useFindAndModify: false,
-//     // useCreateIndex: true,
-// //});
+//mongoose.connect("mongodb://localhost/google-doc-clone", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+//});
 
-// //mongoose.connect("mongodb+srv://Cluster11336:google-d-c@cluster11336.vee1tfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster11336", {
-//     // useNewUrlParser: true,
-//     // useUnifiedTopology: true,
-//     // useFindAndModify: false,
-//     // useCreateIndex: true,
-// //});
-// mongoose.connect("mongodb+srv://dbuser1:pass4google@googledc-db.zylbz6v.mongodb.net/?retryWrites=true&w=majority&appName=googledc-db", {
-//     // useNewUrlParser: true,
-//     // useUnifiedTopology: true,
-//     // useFindAndModify: false,
-//     // useCreateIndex: true,
-// });
+//mongoose.connect("mongodb+srv://Cluster11336:google-d-c@cluster11336.vee1tfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster11336", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+//});
+mongoose.connect("mongodb+srv://dbuser1:pass4google@googledc-db.zylbz6v.mongodb.net/?retryWrites=true&w=majority&appName=googledc-db", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+});
 
-// const io = require("socket.io")(3001, {
-//     cors: {
-//         //origin: "http://localhost:3000",
-//         origin: "google-d-c-c-s.vercel.app",
-//         methods: ["GET", "POST"],
-//     },
-// });
+const io = require("socket.io")(3001, {
+    cors: {
+        //origin: "http://localhost:3000",
+        origin: "google-d-c-c-s.vercel.app",
+        methods: ["GET", "POST"],
+    },
+});
 
-// const defaultValue = "";
+const defaultValue = "";
 
-// io.on("connection", (socket) => {
-//     console.log("connected")
-//     socket.on("get-document", async (documentId) => {
-//         if (!documentId) {
-//             socket.emit("error", "Invalid document ID");
-//             return;
-//         }
+io.on("connection", (socket) => {
+    console.log("connected")
+    socket.on("get-document", async (documentId) => {
+        if (!documentId) {
+            socket.emit("error", "Invalid document ID");
+            return;
+        }
 
-//         try {
-//             const document = await findOrCreateDocument(documentId);
+        try {
+            const document = await findOrCreateDocument(documentId);
 
-//             if (!document) {
-//                 socket.emit("error", "Document not found or could not be created");
-//                 return;
-//             }
+            if (!document) {
+                socket.emit("error", "Document not found or could not be created");
+                return;
+            }
 
-//             socket.join(documentId);
-//             socket.emit("load-document", document.data);
+            socket.join(documentId);
+            socket.emit("load-document", document.data);
 
-//             socket.on("send-changes", (delta) => {
-//                 socket.broadcast.to(documentId).emit("receive-changes", delta);
-//             });
+            socket.on("send-changes", (delta) => {
+                socket.broadcast.to(documentId).emit("receive-changes", delta);
+            });
 
-//             socket.on("save-document", async (data) => {
-//                 await Document.findByIdAndUpdate(documentId, { data });
-//             });
-//         } catch (err) {
-//             console.error(`Error handling get-document event: ${err.message}`);
-//             socket.emit("error", "An error occurred while fetching the document");
-//         }
-//     });
-// });
+            socket.on("save-document", async (data) => {
+                await Document.findByIdAndUpdate(documentId, { data });
+            });
+        } catch (err) {
+            console.error(`Error handling get-document event: ${err.message}`);
+            socket.emit("error", "An error occurred while fetching the document");
+        }
+    });
+});
 
-// async function findOrCreateDocument(id) {
-//     if (!id) return null;
+async function findOrCreateDocument(id) {
+    if (!id) return null;
 
-//     try {
-//         let document = await Document.findById(id);
-//         if (!document) {
-//             document = await Document.create({ _id: id, data: defaultValue });
-//         }
-//         return document;
-//     } catch (err) {
-//         console.error(`Error in findOrCreateDocument: ${err.message}`);
-//         return null;
-//     }
-// }
+    try {
+        let document = await Document.findById(id);
+        if (!document) {
+            document = await Document.create({ _id: id, data: defaultValue });
+        }
+        return document;
+    } catch (err) {
+        console.error(`Error in findOrCreateDocument: ${err.message}`);
+        return null;
+    }
+}
